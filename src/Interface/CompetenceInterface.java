3d0372ed184324c674ef7e1231271f40a924b2e5
Zakaria Nouhou <zakarianouhou418@gmail.com*>
2026-05-08 12:13:32 +0000

    Delete_CompetenceDAO

--- src/Interface/CompetenceInterface.java
+++ src/Interface/CompetenceInterface.java
@@ -1,5 +1,20 @@
 package Interface;
 
+import java.util.List;
+
+import Model.Competence;
+
 public interface CompetenceInterface {
+	
+    void creerCompetence(Competence comp);
+
+    void modifierCompetence(Competence comp);
+
+    void supprimerCompetence(int id);
+
+    List<Competence> trouverTousCompetences();
+
+    Competence trouverCompetenceParId(int id);
+	
 
 }
