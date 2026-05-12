34391abfe2ff812f53e5dd8d1010d79401f8120e
Amadou Oumar N'diaye <amadou14112004@gmail.com>
2026-05-12 11:21:37 +0000

    ActiviteCompetence

---
+++ src/Interface/ActiviteCompetenceDAO.java
@@ -0,0 +1,11 @@
+package Interface;
+
+import Model.Activite;
+import Model.Competence;
+import java.util.List;
+public interface ActiviteCompetenceDAO {
+
+    public void creer(Activite activite);
+    public List<Competence> lire(Activite activite);
+
+}
